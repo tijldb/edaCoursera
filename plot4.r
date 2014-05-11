175e17dba4d193e27fcc111144831af2361ad2ae
tijldb <debacker.tijl@gmail.com>
2014-05-11 18:46:19 +0200

    Last update
    Same axes weren't labeled correctly

--- plot4.r
+++ plot4.r
@@ -11,6 +11,8 @@ dt[,3] = as.numeric(as.character(dt[,3])) # convert the Global Active Power's da
 dt$Sub_metering_1 <- (as.numeric(as.character(dt$Sub_metering_1))) # Convert all Sub_metering to numbers
 dt$Sub_metering_2 <- (as.numeric(as.character(dt$Sub_metering_2)))
 dt$Sub_metering_3 <- (as.numeric(as.character(dt$Sub_metering_3)))
+dt$Voltage <- (as.numeric(as.character(dt$Voltage)))
+dt$Global_reactive_power <- (as.numeric(as.character(dt$Global_reactive_power)))
 dt$dt <- strptime(dt$Datetime, '%d/%m/%Y %H:%M') # create a datetime object 
 
 #plot 4
@@ -20,7 +22,7 @@ plot(dt$dt,dt$Voltage,ylab='Voltage',xlab='datetime',type='line')
 plot(dt$dt,dt$Sub_metering_1,ylab='Energy Sub Metering',xlab='',type='line')
 lines(dt$dt,dt$Sub_metering_2,col='red')
 lines(dt$dt,dt$Sub_metering_3,col='blue')
-legend("topright",legend=c('Sub_metering_1     ','Sub_metering_2    ','Sub_metering_3   '), col=c('black','red','blue'), bty="n",lty=1,lwd=1.5,xjust=1,yjust=0.5,y.intersp=0.6)
+legend("topright",legend=c('Sub_metering_1      ','Sub_metering_2     ','Sub_metering_3    '), col=c('black','red','blue'), bty="n",lty=1,lwd=1.5,xjust=1,y.intersp=0.6,yjust=-1)
 plot(dt$dt,dt$Global_reactive_power,xlab='datetime',ylab='Global_reactive_power',type='line')
 dev.copy(png,'plot4.png',width=480,height=480)
 dev.off()
