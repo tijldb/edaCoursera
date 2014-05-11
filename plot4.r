378ee8f73cd3eb92fd8c04314ac373e29dffd078
tijldb <debacker.tijl@gmail.com>
2014-05-11 20:44:28 +0200

    EDA Project 1
    Finetuned the third graph in the fourth exercise
    Legend is now smaller and on position!

--- plot4.r
+++ plot4.r
@@ -16,13 +16,13 @@ dt$Global_reactive_power <- (as.numeric(as.character(dt$Global_reactive_power)))
 dt$dt <- strptime(dt$Datetime, '%d/%m/%Y %H:%M') # create a datetime object 
 
 #plot 4
-par(mfrow=c(2,2))
+par(mfrow=c(2,2), cex=0.75)
 plot(dt$dt,dt$Global_active_power,ylab='Global Active Power',xlab='',type='line')
 plot(dt$dt,dt$Voltage,ylab='Voltage',xlab='datetime',type='line')
 plot(dt$dt,dt$Sub_metering_1,ylab='Energy sub metering',xlab='',type='line')
 lines(dt$dt,dt$Sub_metering_2,col='red')
 lines(dt$dt,dt$Sub_metering_3,col='blue')
-legend("topright",legend=c('Sub_metering_1      ','Sub_metering_2     ','Sub_metering_3    '), col=c('black','red','blue'), bty="n",lty=1,lwd=1.5,xjust=1,y.intersp=0.6,yjust=-1)
+legend("topright",legend=c('Sub_metering_1      ','Sub_metering_2      ','Sub_metering_3      '), col=c('black','red','blue'), bty="n",lty=1,lwd=1.5,xjust=1,y.intersp=0.6)
 plot(dt$dt,dt$Global_reactive_power,xlab='datetime',ylab='Global_reactive_power',type='line')
 dev.copy(png,'plot4.png',width=480,height=480)
 dev.off()
